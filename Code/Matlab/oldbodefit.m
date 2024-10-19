cbabcddaae58c29a7017522ff3489d7501cef91f
VITTORIO <v.scirli@studenti.unipi.it>
2024-10-19 16:31:49 +0000

    done simulation and op77 plots and made the bodefit ecode able to handle different situation well

--- Code/Matlab/oldbodefit.m
+++ Code/Matlab/oldbodefit.m
@@ -1,24 +1,24 @@
 clear all;
 
 dataPosition = '../../Data/';
-filename = 'dataBode006';
-%filename = 'AD8031';
+filename = 'dataBode008';
+%filename = 'OP77';
 
 mediaposition = '../../Media/';
-medianame = strcat('bodePlotAndFit-Simulation', filename);
+medianame = strcat('bodePlotAndFit-', filename);
 
 flagSave = false;
 flagdB = false;
-flagDeg = false;
-flagLimited = true;
-limit = 80;
+flagDeg = galse;
+flagLimited = false;
+limit = 730;
 
 % data import and creation of variance array
 rawData = readmatrix(strcat(dataPosition, filename, '.txt'));
 
 ff = rawData(:, 1);
 A = rawData(:, 2);
-ph = rawData(:, 8);
+ph = rawData(:, 3);
 
 
 if flagdB
@@ -34,14 +34,31 @@ if flagLimited
 end
 
 
-Ra = 3.2822e3;
-Rb = 1490.3;
-b = Rb / (Ra+Rb);
-G = 100;
+R1 = 1000;
+R2 = 1e5;
+b0 = R2/(R1+R2);
 
-%G0 = 100;
-G0 = G * b;
-f0 = 4e3;
+fc0 = 1e4;
+t0 = 1/(2*pi*fc0);
+A0 = 100;
+
+p0 = [A0, t0];
+
+
+%{
+function y = gain(params, f)
+
+    w = 2 * pi .* f;
+
+    A = params(1) ./ ( 1 + 1i * params(2) .* w);
+    G =  A ./ ( 1 + 1/101 .* A);    
+    y = abs(G);
+
+end
+%}
+
+G0 = 100;
+f0 = 1e4;
 tau0 = 1/(2*pi*f0);
 p0tf = [G0, tau0];
 
@@ -61,11 +78,12 @@ function y = tp(params, f)
 end
 
 
-
+%[beta, R, ~, covbeta] = nlinfit(ff, A, @gain, p0);
+%[beta2, R2, ~, covbeta2] = nlinfit(f2, A2, @gain, p0);
 if flagLimited
-    [beta, R3, ~, covbeta] = nlinfit(f2, A2, @tf, p0tf);
+    [beta3, R3, ~, covbeta3] = nlinfit(f2, A2, @tf, p0tf);
 else
-    [beta, R3, ~, covbeta] = nlinfit(ff, A, @tf, p0tf);
+    [beta3, R3, ~, covbeta3] = nlinfit(ff, A, @tf, p0tf);
 end
 
 
@@ -82,11 +100,14 @@ hold on
 if flagLimited
     loglog(f2, A2, 'v', Color = 'green');
     loglog(f2, tf(p0tf, f2), '--', Color= 'magenta')
-    loglog(f2, tf(beta, f2), '-', Color= 'red');
+    loglog(f2, tf(beta3, f2), '-', Color= 'red');
 else
     loglog(ff, tf(p0tf, ff), '-', Color= 'magenta');
-    loglog(ff, tf(beta, ff), '-', Color= 'red');
+    loglog(ff, tf(beta3, ff), '-', Color= 'red');
 end
+%loglog(ff, abs( gain(p0, ff) ), '--', Color = '#a020f0');
+%loglog(ff, abs( gain(beta, ff) ), '-', Color = 'red');
+%loglog(f2, abs( gain(beta2, f2) ), '-', Color = '#edb120');
 
 
 
@@ -102,42 +123,33 @@ hold on
 if flagLimited
     semilogx(f2, p2, 'v', Color= 'green');
     semilogx(f2, tp(p0tf, f2), '--', Color= 'magenta')
-    semilogx(f2, tp(beta, f2), '-', Color= 'red');
+    semilogx(f2, tp(beta3, f2), '-', Color= 'red');
 else
     semilogx(ff, tp(p0tf, ff), '-', Color= 'magenta');
-    semilogx(ff, tp(beta, ff), '-', Color= 'red');
+    semilogx(ff, tp(beta3, ff), '-', Color= 'red');
 end
 
 grid on
 grid minor
 hold off
 
-title(t, strcat('Gain and Phase of Amplifier - Simulation', filename));
+title(t, strcat('Gain and Phase of Amplifier - Simulation  ', filename));
 
 if flagLimited
     legend(ax1, 'Original Data', 'Fit Data', 'model - p0', 'model - fit', Location= 'ne');
 else
     legend(ax1, 'Original Data', 'model - p0', 'model - fit', Location= 'ne');
 end
-
-
 linkaxes([ax1 ax2], 'x')
 ylabel(ax1, 'Gain [pure]');
 ylabel(ax2, 'Phase [radians]');
 xlabel(ax2, 'Frequency [Hz]');
 
+
 yticks(ax2, [-pi, -0.75*pi, -pi/2, -pi/4, 0, pi/4, pi/2, 0.75*pi, pi])
 yticklabels(ax2, {'-pi', '-3/4\pi', '-\pi/2', '-\pi/4', '0', '\pi/4', '\pi/2', '3/4\pi', '\pi'})
 
-dim = [.08 .55 .3 .3];
-str = strcat('G_{\omega \rightarrow 0} = ', sprintf('%.2f', beta(1)/b ) );
-annotation('textbox',dim,'String',str,'FitBoxToText','on', 'Interpreter', 'tex', 'BackgroundColor', 'white');
-dim = [.08 .5 .3 .3];
-str = sprintf('f_c = %.2e Hz', 1/(2*pi*beta(2) ) ) ;
-annotation('textbox',dim,'String',str,'FitBoxToText','on', 'Interpreter', 'tex', 'BackgroundColor', 'white');
-dim = [.08 .45 .3 .3];
-str = sprintf('GBWP = %.2e', 1/(2*pi*beta(2)) * beta(1)/b  ) ;
-annotation('textbox',dim,'String',str,'FitBoxToText','on', 'Interpreter', 'tex', 'BackgroundColor', 'white');
+
 
 
 
